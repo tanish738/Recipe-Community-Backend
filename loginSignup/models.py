790edb6079c2f614b3414a93f2a07f1d3ada2931
Tanish <patwatanish10@gmail.com>
2021-12-05 20:07:33 +0530

    Adding views to serializers

---
+++ loginSignup/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.text import slugify
+
+#for token 
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+# Create your models here.
+
+
+#this function goes to static/images as that is the MEDIA_ROOT in settings.py
+#it then uploads images of users in a dir userProfile/<userEmail>/<uploaded_image>
+def upload_path_handler(instance, filename): 
+    return "userProfile/{title}/{file}".format(
+        title=instance.myUser.email, file=filename
+    )
+
+
+class MyUser(AbstractUser):
+    email           = models.EmailField(unique=True, blank = False)
+    username        = models.CharField(max_length = 100,unique=True, blank = False)
+    first_name      = models.CharField(max_length = 100, blank = True)
+    last_name       = models.CharField(max_length = 100, blank = True)
+    profile_picture = models.ImageField(default = "default_user_profile.png", upload_to = upload_path_handler) 
+    Created         = models.DateTimeField(null = True, blank = False, auto_now_add = True)
+    # KEEP IT DEFAULT TRUE
+    is_active       = models.BooleanField(default = True) #so that we can activate using SMTP # keep it true until running the server
+    is_chef         = models.BooleanField(default = False) #for V2
+    bio             = models.TextField(blank = True)
+    slug            = models.SlugField(max_length=100, unique=True)
+
+    USERNAME_FIELD = 'email' #login signup with email instead of username
+    REQUIRED_FIELDS=['username']#admin creation
+    
+
+    def __str__(self):
+        return self.email
+
+
+    #will display a summary of bio upto 50 words
+    def summary(self): 
+        return self.bio[:50] + "..."
+    
+    
+    #save the slug field before saving myUser model
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.username)
+        super(MyUser, self).save(*args, **kwargs)
+
+
+#function to create token for new user
+@receiver(post_save, sender = settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance = None, created = False, **kwargs):
+    if created:
+        Token.objects.create(user = instance)
---
+++ main/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render\
+
+
+#FOR API AND REST_FRAMEWORK
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework import generics
+
+#FOR MODELS
+from .models import *
+
+#FOR SERILAZERS 
+from .serializers import *
+
+# Create your views here.
+
+class RecipeApi(generics.ListCreateAPIView):
+    queryset =Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+"""
+@api_view(['GET', 'POST'])
+def customer_list(request):
+    if request.method=="GET":
+        customer=Customer.objects.all()
+        serializer=CustomerSerializer(customer, many=True)
+        return Response(serializer.data)
+    elif request.method=="POST":
+        data=JSONParser().parse(request)
+        serializer=CustomerSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+"""
+class IngredientsApi(generics.ListCreateAPIView):
+    queryset= Ingredients.objects.all()
+    serializer_class=IngredientsSerializer
+
+
+class FAQsApi(generics.ListCreateAPIView):
+    queryset=Faqs.objects.all()
+    serializer_class=FaqsSerializer
