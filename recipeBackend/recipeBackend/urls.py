59198d1548007c54e84f1804939a5c615f8bc5ad
Arpit472002 <sarpit623@gmail.com>
2021-12-06 18:03:26 +0530

    SwaggerDocs added

--- recipeBackend/recipeBackend/urls.py
+++ recipeBackend/recipeBackend/urls.py
@@ -19,10 +19,29 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Recipe API",
+      default_version='v1',
+      description="Users can upload their recipes and try other's uploaded recipe",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login-signup/', include('loginSignup.urls')),
-    path('main/', include('main.urls'))
+    path('main/', include('main.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
